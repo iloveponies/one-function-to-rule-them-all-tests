44d15a06ddb06129a2687d475bd7002d04091fdb
Jani Rahkola <jani.rahkola@iki.fi>
2013-11-11 11:46:43 +0200

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# one-function-to-rule-them-all
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject iloveponies.tests/one-function-to-rule-them-all "0.1.0-SNAPSHOT"
+  :description "tests for one-function-to-rule-them-all"
+  :url "http://github.com/iloveponies/one-function-to-rule-them-all"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [midje "1.5.1"]
+                 [midje-grader "0.1.0-SNAPSHOT"]])
